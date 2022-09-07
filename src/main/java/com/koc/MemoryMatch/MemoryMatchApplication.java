4e81498ab8ea978ae876eb79aa42143103dbcd6d
Matt McCain <mmccain814@gmail.com>
2022-09-07 16:53:43 -0400

    project setup

---
+++ src/main/java/com/koc/MemoryMatch/MemoryMatchApplication.java
@@ -0,0 +1,13 @@
+package com.koc.MemoryMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koc/MemoryMatch/MemoryMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koc.MemoryMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
