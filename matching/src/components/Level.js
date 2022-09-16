834dc57225ecb4659fb1a9f761ebb2ceaf16199f
Win Thurein Myint <winthureinmyint07@gmail.com>
2022-09-16 16:19:24 +0700

    POST /game api implement

--- matching/src/components/Level.js
+++ matching/src/components/Level.js
@@ -3,7 +3,7 @@ export default function Level({ getCardsInfo }) {
     return (
         <div className="difficulity">
             {/* <h3>Choose the difficulity</h3> */}
-            <button onClick={() => getCardsInfo(2, "Easy")}>Easy </button>
+            <button onClick={() => getCardsInfo(10, "Easy")}>Easy </button>
             <button onClick={() => getCardsInfo(18, "Medium")}>Medium </button>
             <button onClick={() => getCardsInfo(26, "Hard")}>Hard </button>
         </div>
