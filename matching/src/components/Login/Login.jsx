5fac19069f633122f250277abf08d288d629549a
Matt McCain <mmccain814@gmail.com>
2022-09-17 21:24:22 -0400

    Started Login Form

--- matching/src/components/Login/Login.jsx
+++ matching/src/components/Login/Login.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import './Login.css';
 
-const Login = () => {
+const Login = (props) => {
+
   return (
     <>
     <h1>Login</h1>
+    <div className="formContainer">
+    <form>
+      <label>Username:</label>
+      <input type="text" placeholder='Username' />
+      <label>Password:</label>
+      <input type='password' placeholder='Password' />
+      <input type='submit' className='button' value='Login' />
+    </form>
+    </div>
     </>
   )
 }
